b54fe55982d419b5aa024266b7a33ae859ee84ba
huseyn777 <huseynfl7@gmail.com>
2022-12-09 12:09:57 +0300

    angular project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import {ButtonModule} from 'primeng/button';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
